590da306f252751303b20a22d838ac7f31022418
morikne36 <morikne11450@gmail.com>
2021-04-18 03:30:10 +0900

    main

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
-$(function(){
+window.onload = function(){
+	fade_effect();
+
 	$(window).scroll(function (){
+		fade_effect();
+	});
+
+	function fade_effect(){
 		$('.fadein').each(function(){
 			var elemPos = $(this).offset().top;
 			var scroll = $(window).scrollTop();
